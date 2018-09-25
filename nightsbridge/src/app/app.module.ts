35e948364d53ff6447f508b970886ac2fd82bbfe
jamesthealien <james@cowboyaliens.com>
2018-09-25 19:48:03 +0200

    Added initial components and style scss

--- nightsbridge/src/app/app.module.ts
+++ nightsbridge/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { ButtonComponent } from './common/button/button.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { MissingComponent } from './pages/missing/missing.component';
+import { StyleGuideComponent } from './pages/style-guide/style-guide.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ButtonComponent,
+    LandingComponent,
+    MissingComponent,
+    StyleGuideComponent
   ],
   imports: [
     BrowserModule,
